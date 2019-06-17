bf7452f65f56f61c8aaec7d9ef9b515c0443b41d
Johannes <jcmf.schule@gmail.com>
2019-06-17 07:08:30 +0200

    Add Unit tests
    HPBS48 still broken. Fuck.

---
+++ test/runtests.jl
@@ -0,0 +1,69 @@
+using LakeBiquads, DelimitedFiles, DSP, Test
+fs=96e3
+
+#for testplot
+include("utils.jl")
+
+function compareReference(refName, testFilter; fStart = nothing, fStop=22e3, plot=false)
+    plot && testplot(refName, testFilter)
+    input = readdlm(refName, ';', skipstart=1)
+    fIn = input[:,1]
+    dBIn = input[:,2]
+    phaseIn = input[:,3]
+    if fStart==nothing
+        fStart = minimum(fIn)
+    end
+    if fStop==nothing
+        fStop = maximum(fIn)
+    end
+    for i in eachindex(fIn)
+        if fIn[i] > fStop
+            break
+        elseif fIn[i] >= fStart
+            #print(i, " ")
+            if isapprox(dBIn[i], 20*log10(abs(freqz(testFilter, fIn[i], fs))), atol=.07) != true
+                println(refName, ": failed level at frequency ", fIn[i], " Hz")
+                return false
+            end
+            if isapprox(phaseIn[i], angle(freqz(testFilter, fIn[i], fs))/pi*180, atol=1) != true
+                println(refName, ": failed phase at frequency ", fIn[i], " Hz")
+                return false
+            end
+        end
+    end
+    return true
+end
+
+@testset "all Tests" begin
+    @testset "Reference comparisons" begin
+        @test compareReference("AP 10k 1 3.csv", AllPass(10e3, 1, 3))
+        @test compareReference("AP 10k 2 2.csv", AllPass(10e3, 2, 2))
+        @test compareReference("P 10k 15 2.csv", Peak(10e3, 15, 2))
+        @test compareReference("P 100 -6 1.csv", Peak(100, -6, 1))
+        @test compareReference("LS 10k 15 2.5.csv", LowShelf(10e3, 15, 2.5))
+        @test compareReference("HS 5k 10 2.csv", HighShelf(5e3, 10, 2))
+
+        #now the LowHighPass tests, take care to adjust fStart/fStop individually to surpress Noise
+        @test_broken compareReference("HPBS48 1k.csv", HighPass(:BS, 1e3, 48), fStart=500, plot=false) #fails hard
+        @test compareReference("HPBT12 40.csv", HighPass(:BT, 40, 12), fStart=16, plot=false) #curves diverge towards low frequencies
+        @test compareReference("HPLR48 1k.csv", HighPass(:LR, 1e3, 48), fStart=500, plot=false)
+        @test compareReference("LPBS6 1k.csv", LowPass(:BS, 1e3, 6), plot=false)
+        @test compareReference("LPBS12 1k.csv", LowPass(:BS, 1e3, 12), fStop=10e3, plot=false)
+        @test compareReference("LPBS18 1k.csv", LowPass(:BS, 1e3, 18), fStop=6e3, plot=false)
+        @test compareReference("LPBS24 1k.csv", LowPass(:BS, 1e3, 24), fStop=4e3, plot=false)
+        @test compareReference("LPBS24 1k.csv", LowPass(:BS, 1e3, 24), fStop=4e3, plot=false)
+        @test compareReference("LPBS30 1k.csv", LowPass(:BS, 1e3, 30), fStop=2.5e3, plot=false)
+        @test compareReference("LPBS36 1k.csv", LowPass(:BS, 1e3, 36), fStop=2.5e3, plot=false)
+        @test compareReference("LPBS42 1k.csv", LowPass(:BS, 1e3, 42), fStop=2e3, plot=false)
+        @test compareReference("LPBS48 1k.csv", LowPass(:BS, 1e3, 48), fStop=2e3, plot=false)
+        @test compareReference("LPBT6 10k HPLR12 100.csv", LowPass(:BT, 10e3, 6)*HighPass(:LR, 100, 12), fStart=30, plot=false) #divergence towards low frequencies
+        @test compareReference("LPBT24 10k.csv", LowPass(:BT, 10e3, 24), plot=false)
+        @test compareReference("LPBT48 1k.csv", LowPass(:BT, 1e3, 48), fStop=2e3, plot=false)
+    end
+
+    @testset "Batch conversion" begin
+        EQBiquads("testEQFile.txt") #just check for errors on execution
+        #TODO
+        #read resulting file and compare to single calculated filters
+    end
+end
--- test/utils.jl
+++ test/utils.jl
@@ -1,5 +1,5 @@
 using PyPlot
-frequencies = range(20, 20000, length=10000)
+frequencies = range(10, 22000, length=10000)
 
 function show_grid()
 	minorticks_on()
@@ -8,13 +8,13 @@ function show_grid()
 	xlabel("f in Hz")
 end
 
-function magplot(myfilter)
+function magplot(myfilter, frequencies=frequencies)
 	plot(frequencies, 20*log10.(abs.(freqz(myfilter, frequencies, fs))), label="Level/dB")
 	semilogx()
 	show_grid()
 end
 
-function phaseplot(myfilter)
+function phaseplot(myfilter, frequencies=frequencies)
 	plot(frequencies, angle.(freqz(myfilter, frequencies, fs)), label="Phase/rad")
 	semilogx()
 	show_grid()
@@ -30,8 +30,8 @@ function testplot(filename, myfilter)
 	input = readdlm(filename, ';', skipstart=1)
 	plot(input[:,1], input[:,2], label="ref Level/dB")
 	plot(input[:,1], input[:,3]/180*pi, label="ref Phase/rad")
-	magplot(myfilter)
-	phaseplot(myfilter)
+	magplot(myfilter, input[:,1])
+	phaseplot(myfilter, input[:,1])
 	legend()
 end
 
