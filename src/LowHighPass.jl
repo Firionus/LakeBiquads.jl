5e8805cbe7330695ed7a2c16dfba33a9fb6090af
Johannes <jcmf.schule@gmail.com>
2019-06-22 14:26:58 +0200

    add some different methods for LPBS frequency normalisation and working HPBS methods

--- src/LowHighPass.jl
+++ src/LowHighPass.jl
@@ -84,11 +84,15 @@ function Bessel(N::Int)
 	denominator = [b(n)|>Float64 for n=N:-1:0]
 	nominator = zeros(N+1)
 	nominator[end] = denominator[end]
+
 	if isodd(N)
 		nominator = -nominator
 	end
+
 	myfilter = PolynomialRatio(nominator, denominator)
 	myfilter2 = convert(ZeroPoleGain, myfilter)
+
+
 	#construct function for NLsolve to find -3dB point
 	myfilterdB(w) = 20*log10.(abs.(freqs(myfilter2, w)))
 	function func!(dB, w)
@@ -104,14 +108,100 @@ function Bessel(N::Int)
 
 	correctionFactor = LakeCorrectionFactor*BesselCorrectionFactor
 
+
+
+
+	#=
+	#roughly hits frequency normalisation of Lake, but still inversion while converting
+	#to highpass results in peaky GroupDelay
+
+	#the correction fucks with the inversion in conversion to HighPass
+	#need to properly normalize the Bessel filter for conversion to HighPass to work properly I guess
+
+	#construct function for NLsolve to find halfPhase point
+	myfilterphase(w) = angle.(freqs(myfilter2,w)) #no unwrap -> not monotonic! -> can't really work...
+	halfPhase = rem2pi(angle.(freqs(myfilter2, 0))-N*pi/4, RoundNearest)
+	#println(halfPhase)
+	#wTest = range(0, stop=10, length=10000)
+	#plot(wTest, angle.(freqs(myfilter2, wTest)))
+	function func!(phase, w)
+		phase[1] = myfilterphase(w[1])-halfPhase
+	end
+
+	#need to guess start right to get into right branch
+	if N<= 4
+		start = 2.
+	else
+		start=5.
+	end
+
+	res = nlsolve(func!, [start], autodiff=:forward)
+	wHalfPhase = res.zero[1]
+	#println(wHalfPhase)
+	correctionFactor = 1/wHalfPhase
+	=#
+
+	#=
+
+	#sucks completely
+
+	#another new frequency normalization: same asymptote as Butterworth prototype of same order
+	btproto = Butterworth(N)
+	wRef = 1e8
+	refMagnitude = abs(freqs(btproto, wRef))
+
+	myfilterMagnitude(w) = abs(freqs(myfilter2, w))
+	function func!(magn, w)
+		magn[1] = myfilterMagnitude(w[1])-refMagnitude
+	end
+	res = nlsolve(func!, [wRef], autodiff=:forward)
+	wRefMagnitude = res.zero[1]
+
+	correctionFactor = wRef/wRefMagnitude
+	=#
+
+	#=
+	#new freq normalization that makes GroupDelay at w=1 equal to
+	# GD_max * N/(N+1)
+	myphase(w) = angle(freqs(myfilter2, w))
+	using ForwardDiff
+	myfilterGD(w) = -ForwardDiff.derivative(myphase, w)
+	GD0 = myfilterGD(0.)
+	println("order ", N)
+	println(GD0)
+	targetGD = GD0*N/(N+1)
+	println(targetGD)
+	function func!(gd, w)
+		gd[1] = myfilterGD(w[1])-targetGD
+	end
+	res = nlsolve(func!, [2.5], autodiff=:forward)
+	targetGDFrequency = res.zero[1]
+	println(targetGDFrequency)
+
+
+	correctionFactor = 1/targetGDFrequency
+
+	correctionFactor *= .1
+	=#
+
 	myfilter3 = ZeroPoleGain(myfilter2.z .* correctionFactor,
 							 myfilter2.p .* correctionFactor,
 				myfilter2.k * correctionFactor^(length(myfilter2.p)-length(myfilter2.z)))
 	return myfilter3
 end
 
+function BesselInvert(prototype)
+	N = length(prototype.p)
+	if isodd(N)
+		newGain = -prototype.k
+	else
+		newGain = prototype.k
+	end
+	return ZeroPoleGain(fill(0+0im, N), prototype.p, newGain)
+end
+
 function PassFilter(responsetype, type, dBperOct)
-	if dBperOct in [6,12,18,24,30,36, 42, 48]
+	if dBperOct in [6,12,18,24,30,36,42,48]
 		Poles = dBperOct/6|>Int
 	else
 		error("dBperOct is not one of [6,12,18,24,30,36,42,48]")
@@ -122,7 +212,13 @@ function PassFilter(responsetype, type, dBperOct)
 		Filter = convert(SecondOrderSections, digitalfilter(responsetype, designmethod))
 	elseif type == :BS
 		designmethod = Bessel(Poles)
-		Filter = convert(SecondOrderSections, digitalfilter(responsetype, designmethod))
+		if typeof(responsetype)<:Lowpass
+			Filter = convert(SecondOrderSections, digitalfilter(responsetype, designmethod))
+		else #Highpass
+			hp_responsetype = Lowpass(responsetype.w)
+			hpbs_designmethod = BesselInvert(designmethod)
+			Filter = convert(SecondOrderSections, digitalfilter(hp_responsetype, hpbs_designmethod))
+		end
 	elseif type == :LR
 		if Poles in [2,4,6,8]
 			designmethod = Butterworth(Poles/2|>Int)
