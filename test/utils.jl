d9a088b5c2128184278c6b719dddf9e6c7fe4e9b
Johannes <jcmf.schule@gmail.com>
2019-06-22 14:20:12 +0200

    add gdplot and csvGDplot to utils & slight change to standard frequencies

--- test/utils.jl
+++ test/utils.jl
@@ -1,6 +1,6 @@
 using PyPlot
-frequencies = range(10, 22000, length=10000)
-
+frequencies = range(16, 20000, length=10000)
+fs = 96e3
 function show_grid()
 	minorticks_on()
 	grid(which="major", color="gray", linestyle="-")
@@ -16,6 +16,26 @@ end
 
 function phaseplot(myfilter, frequencies=frequencies)
 	plot(frequencies, angle.(freqz(myfilter, frequencies, fs)), label="Phase/rad")
+	semilogx()
+	show_grid()
+end
+
+function gdplot(myfilter, frequencies=frequencies)
+	deltaf = frequencies.step.hi
+	gdfrequencies = range(frequencies[1]+deltaf/2, frequencies[end]-deltaf/2, length=frequencies.len-1)
+	phaseresponse = angle.(freqz(myfilter, frequencies, fs))|>unwrap
+	gd = -diff(phaseresponse)/deltaf
+	plot(gdfrequencies, gd)
+	semilogx()
+	show_grid()
+end
+
+function csvGDplot(filename)
+	input = readdlm(filename, ';', skipstart=1)
+	deltaf = [input[i+1,1]-input[i,1] for i=1:size(input,1)-1]
+	gd = -diff(unwrap(input[:,3]/180*pi))./deltaf
+	fAxis = [(input[i+1,1]+input[i,1])/2 for i=1:size(input,1)-1]
+	plot(fAxis, gd)
 	semilogx()
 	show_grid()
 end
